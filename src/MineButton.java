0c304b757a80f217e493e2a44b3bfdf38cecfd18
Jovan Nikolic <jnikolic7124si@raf.rs>
2024-10-30 11:37:14 +0100

    first commit

---
+++ src/MineButton.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MineButton extends JButton {
+    int rNumber;
+    int cNumber;
+
+    public MineButton(int rNumber, int cNumber) {
+        this.rNumber = rNumber;
+        this.cNumber = cNumber;
+    }
+}
